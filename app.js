b2bbe4c8daa9c04ef1bd84e1d505bfa1b38bf6a3
viseth89 <viseth89@gmail.com>
2018-12-18 08:15:46 -0600

    finishing section 3, about to add bootstrap and nav bar

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+
+const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// How middleware WOrks
+app.use(function(req, res, next){
+  console.log(Date.now());
+  req.name = 'Viseth SEN';
+  next()
+});
+
+// Index Route
+app.get('/', (req, res) =>{
+  const title='Welcome';
+  res.render('index', {
+    title:title
+  });
+});
+
+// About Route
+app.get('/about', (req, res) =>{
+  res.render('ABOUT');
+});
+
+
+
+const port = 5000;
+
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`);
+});
