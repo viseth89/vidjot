5ced629f516e41628275e2015a7f7eeeac6104a2
viseth89 <viseth89@gmail.com>
2018-12-18 12:06:20 -0600

    working on video 17, created idea model

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const express = require('express');
-const exphbs  = require('express-handlebars');
+const express   = require('express');
+const exphbs    = require('express-handlebars');
+const mongoose  = require('mongoose')
 
 const app = express();
+
+// Map global promise - get rid of warning (depreciated)
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjog-dev', {
+  useNewUrlParser: true
+})
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
