3f05e03340f5ae0928fa926d8493480be92c1b62
viseth89 <viseth89@gmail.com>
2018-12-19 15:12:16 -0600

    db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
   module.exports = {mongoURI:
-    'mongodb://viseth:Safevase1@ds139614.mlab.com:39614/vidjot-prod'
+    'mongodb://vibol:vibol1@ds139614.mlab.com:39614/vidjot-prod'
   }
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
