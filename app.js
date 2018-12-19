72ccefbcf151f7c2462efff798e4c46cf5d090c4
viseth89 <viseth89@gmail.com>
2018-12-19 11:41:42 -0600

    working on access control

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride    = require('method-override')
 const flash             = require('connect-flash');
 const session           = require('express-session');
 const bodyParser        = require('body-parser');
+const passport          = require('passport');
 const mongoose          = require('mongoose');
 
 const app = express();
@@ -12,6 +13,9 @@ const app = express();
 // Load Routes
 const ideas = require('./routes/ideas')
 const users = require('./routes/users')
+
+// Passport Config
+require('./config/passport')(passport)
 
 // Map global promise - get rid of warning (depreciated)
 mongoose.Promise = global.Promise;
@@ -39,12 +43,16 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Method override middleware
 app.use(methodOverride('_method'))
 
-// Express Session
+// Express Session middleware
 app.use(session({
   secret: 'keyboard cat',
   resave: true,
   saveUninitialized: true,
 }))
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(flash());
 
@@ -53,6 +61,7 @@ app.use(function(req, res, next){
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 })
 
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
-const express     = require('express');
-const mongoose    = require('mongoose');
-const bcrypt      = require('bcryptjs');
-// const passport    = require('passport');
-const router      = express.Router();
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const router = express.Router();
 
 
 // Load User Model
@@ -19,6 +19,15 @@ router.get('/register', (req, res) => {
   res.render('users/register')
 });
 
+// Login Form POST
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/ideas',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
+})
+
 // Register Form POST
 router.post('/register', (req, res) => {
   // console.log(req.body);
@@ -26,55 +35,59 @@ router.post('/register', (req, res) => {
 
   let errors = []
 
-  if(req.body.password != req.body.password2){
-    errors.push({text: 'Passwords do not match'})
+  if (req.body.password != req.body.password2) {
+    errors.push({ text: 'Passwords do not match' })
   }
 
-  if(req.body.password < 4){
-    errors.push({text: 'Password must be at least 4 characters'})
+  if (req.body.password < 4) {
+    errors.push({ text: 'Password must be at least 4 characters' })
   }
 
-  if(errors.length > 0){
-    res.render('users/register',{ 
-    errors: errors,
-    name:req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    password2: req.body.password2
-  });
-} else {
-  User.findOne({email: req.body.email})
-    .then(user => {
-      if(user){
-        req.flash('error_msg', 'Email already registered')
-        res.redirect('/users/register')
-      } else {
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password
-        })
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt,(err, hash) => {
-            if(err) throw err;
-            newUser.password = hash;
-            newUser.save()
-              .then(user => {
-                req.flash('success_msg', 'You are now registered and can log in');
-                res.redirect('/users/login');
-              })
-              .catch(err => {
-                console.log(err);
-                return;
-              })
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (user) {
+          req.flash('error_msg', 'Email already registered')
+          res.redirect('/users/register')
+        } else {
+          const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
           })
-        })
-      }
-    })
-  
-}
-
-
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser.password = hash;
+              newUser.save()
+                .then(user => {
+                  req.flash('success_msg', 'You are now registered and can log in');
+                  res.redirect('/users/login');
+                })
+                .catch(err => {
+                  console.log(err);
+                  return;
+                })
+            })
+          })
+        }
+      })
+  }
 });
+
+// Logout User
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+})
 
 module.exports = router;
